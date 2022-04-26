3d42dd5642556123689afa05f22a660c9acf9fc6
Nisha pela <84078164+PelaNisha@users.noreply.github.com>
2022-04-26 22:40:57 +0545

    Update tweet.py

--- tweet.py
+++ tweet.py
@@ -25,3 +25,16 @@ def recent_tweets_of_user():
     query_params = {'query': '(from:'+twitter_user+' -is:retweet)','tweet.fields': 'author_id' , 'max_results':15}
     json_response = connect_to_endpoint(search_url, query_params)
     print(json.dumps(json_response, indent=4, sort_keys=True))
+
+
+def tweets_with_keywords_hashtags():
+    keywords_a = input("Enter the keyqords: ")
+    keywords_b = input("Enter the keyqord 2: ")
+
+    search_url = "https://api.twitter.com/2/tweets/search/recent"
+
+    query_params = {'query':'('+keywords_a+' OR '+keywords_b+') lang:en -birthday -is:retweet'}
+    json_response = connect_to_endpoint(search_url, query_params)
+    print(json.dumps(json_response, indent=4, sort_keys=True))
+
+tweets_with_keywords_hashtags()    
