dd59405785e5bbd9929208759cc9c3e77db600f3
Nisha pela <84078164+PelaNisha@users.noreply.github.com>
2022-05-08 18:24:41 +0545

    New commit

--- twitter_bot2.py
+++ twitter_bot2.py
@@ -22,9 +22,11 @@ def get_guest_token():
 	return token
 
 
-def get_user_data():	
+def get_user_data(username):	
 	token = read_file('token.json')
-	url = 'https://twitter.com/i/api/graphql/Bhlf1dYJ3bYCKmLfeEQ31A/UserByScreenName?variables={"screen_name":"narendramodi","withSafetyModeUserFields":true,"withSuperFollowsUserFields":true}'
+	url = 'https://twitter.com/i/api/graphql/Bhlf1dYJ3bYCKmLfeEQ31A/UserByScreenName?variables={"screen_name":"'+username+'","withSafetyModeUserFields":true,"withSuperFollowsUserFields":true}'
+
+	
 	Headers = {"Authorization":bearer_token,"User-Agent":user_agent,"x-guest-token" : token}
 	response = requests.get(url, headers=Headers)
 	if response.status_code != 200:
@@ -33,7 +35,7 @@ def get_user_data():
 		f = open('token.json', 'r+')
 		f.truncate(0) 
 		save_to_file(token, 'token.json')
-		return get_user_data()
+		return get_user_data(username)
 		
 	# print("Status Code", response.status_code)
 	return response.json()
@@ -44,13 +46,48 @@ def save_to_file(final_result,filename):
 		json.dump(final_result, f, indent = 2)
 
 
-def get_filtered_data(x):
+def get_user_profile_data(x):
 	y = x['data']['user']['result']['legacy']
 	filtered_output = {'created at':y['created_at'], "description":y["description"],"followers_count":y["followers_count"],
 			 "friends_count":y[ "friends_count"],"location":y["location"],"name":y["name"],}
 	return filtered_output
 
+def get_tweets():
+	token = read_file('token.json')
+	url = 'https://twitter.com/i/api/graphql/07VfD4dpV9RcW5dsbCjYuQ/UserTweets?variables={"userId":"1349149096909668363","count":40,"includePromotedContent":true,"withQuickPromoteEligibilityTweetFields":true,"withSuperFollowsUserFields":true,"withDownvotePerspective":false,"withReactionsMetadata":false,"withReactionsPerspective":false,"withSuperFollowsTweetFields":true,"withVoice":true,"withV2Timeline":true,"__fs_responsive_web_like_by_author_enabled":false,"__fs_dont_mention_me_view_api_enabled":true,"__fs_interactive_text_enabled":true,"__fs_responsive_web_uc_gql_enabled":false,"__fs_responsive_web_edit_tweet_api_enabled":false}'
+	Headers = {"Authorization":bearer_token,"User-Agent":user_agent,"x-guest-token" : token}
+	response = requests.get(url, headers=Headers)
+	if response.status_code != 200:
+		print("hello")
+		token = get_guest_token()
+		f = open('token.json', 'r+')
+		f.truncate(0) 
+		save_to_file(token, 'token.json')
+		return get_tweets()
+		
+	# print("Status Code", response.status_code)
+	return response.json()
+	# save_to_file(response, 'tweets.json')
 
-x = get_user_data()
-o = get_filtered_data(x)
-save_to_file(o, 'file.json')
+
+def get_filtered_tweets(x):
+	li = []
+	y = x['data']['user']['result']["timeline_v2"]["timeline"]['instructions'][1]['entries']
+	# print(len(y))
+	# s = y['entries']
+	for i in range(0,5):
+		p = y[i]["content"]['itemContent']['tweet_results']['result']['legacy']
+		# print(p)
+		filtered_output = {"created_at":p["created_at"],"full_text":p["full_text"], "retweet_count":p["retweet_count"],"reply_count":p["reply_count"],
+				"quote_count":p["quote_count"]}
+		li.append(filtered_output)		
+	# print(li)
+	return li
+
+
+x = get_user_data("POTUS")
+# o = get_user_profile_data(x)
+# save_to_file(o, 'file.json')
+o = get_tweets()
+a= get_filtered_tweets(o)
+save_to_file(a, 'tweets.json')
