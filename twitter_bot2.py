a6c19d8954eb798c31f01a4f78c5a6bee8fc71be
Nisha pela <84078164+PelaNisha@users.noreply.github.com>
2022-05-08 15:49:15 +0545

    Update twitter_bot2.py

--- twitter_bot2.py
+++ twitter_bot2.py
@@ -7,25 +7,40 @@ bearer_token = os.environ.get("BEARER_TOKEN")
 user_agent = os.environ.get("User-Agent")
 
 
+def read_file(file):
+	with open(file, 'r') as f:
+		data = json.loads(f.read())
+		return data
+		
+
 def get_guest_token():
 	url = "	https://api.twitter.com/1.1/guest/activate.json"
 	Headers = {"Authorization":bearer_token, 'User-Agent':'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:99.0) Gecko/20100101 Firefox/99.0','content-type':'application/x-www-form-urlencoded'}
-	x = requests.post(url, headers=Headers)
-	b = x.json()
+	response = requests.post(url, headers=Headers)
+	b = response.json()
 	token = b['guest_token']
 	return token
 
 
-def get_info(token):	
+def get_info():	
+	token = read_file('token.json')
 	url = 'https://twitter.com/i/api/graphql/Bhlf1dYJ3bYCKmLfeEQ31A/UserByScreenName?variables={"screen_name":"narendramodi","withSafetyModeUserFields":true,"withSuperFollowsUserFields":true}'
 	Headers = {"Authorization":bearer_token,"User-Agent":user_agent,"x-guest-token" : token}
 	response = requests.get(url, headers=Headers)
+	if response.status_code != 200:
+		print("hello")
+		token = get_guest_token()
+		f = open('token.json', 'r+')
+		f.truncate(0) 
+		save_to_file(token, 'token.json')
+		return get_info()
+		
 	# print("Status Code", response.status_code)
 	return response.json()
 
 
-def save_to_file(final_result):
-	with open('file.json', "w+") as f:
+def save_to_file(final_result,filename):
+	with open(filename, "w+") as f:
 		json.dump(final_result, f, indent = 2)
 
 
@@ -35,8 +50,7 @@ def get_user_data(x):
 			 "friends_count":y[ "friends_count"],"location":y["location"],"name":y["name"],}
 	return output
 
-	
-a = get_guest_token()
-x = get_info(a)
+
+x = get_info()
 o = get_user_data(x)
-save_to_file(o)
+save_to_file(o, 'file.json')
